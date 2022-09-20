b2b9631389aeecc60f2c6bd6aff8a9b220a47c26
Serkan Sonmez <serkan.sonmez@capgemini.com>
2022-09-20 12:11:27 -0400

    back_end

---
+++ billingApp_backEnd/src/main/java/com/bilingApp/backend/BillingAppBackEndApplication.java
@@ -0,0 +1,15 @@
+package com.bilingApp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingAppBackEndApplication.class, args);
+
+		System.out.println("git hub");
+	}
+
+}
---
+++ billingApp_backEnd/src/test/java/com/bilingApp/backend/BillingAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilingApp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
