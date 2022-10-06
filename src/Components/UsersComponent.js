3771477501b7297fd6b14776d8686e3a8fe4e2f5
Serkan Sonmez <serkan.sonmez@capgemini.com>
2022-10-06 17:13:16 -0400

    User info addded to User component

--- src/Components/UsersComponent.js
+++ src/Components/UsersComponent.js
@@ -1,6 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const UsersComponent = () => {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    await axios
+      .get(process.env.REACT_APP_BASE_USERS)
+      .then((response) => {
+        setUsers(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div
       style={{
@@ -8,7 +24,26 @@ const UsersComponent = () => {
         marginTop: "-30px",
         minHeight: "100vh",
       }}
-    ></div>
+    >
+      <div style={{ padding: "10%", marginLeft: "20%", marginRight: "20%" }}>
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Users' Emails</th>
+            </tr>
+          </thead>
+          {users.map((item, key) => (
+            <tbody key={key}>
+              <tr>
+                <th scope="row">{item.id}-</th>
+                <td className="text-left">{item.email}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    </div>
   );
 };
 
